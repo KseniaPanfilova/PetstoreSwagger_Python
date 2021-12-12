aace7e91718cbcd3d1a90e08b749aa17d5ba5242
kpanfilova <kseniapanfilova@gmail.com>
2021-12-12 16:41:58 +0300

    updated python script

--- PetstoreAPI_PythonTests.py
+++ PetstoreAPI_PythonTests.py
@@ -7,97 +7,80 @@ import time
 
 fake = Faker() 
 
-userName = fake.user_name()
+user_name = fake.user_name()
 password = fake.password()
-userID = randint(1, 1000)
-firstName = fake.first_name()
-lastName = fake.last_name()
+user_id = randint(1, 1000)
+first_name = fake.first_name()
+last_name = fake.last_name()
 email = fake.email()
 phone = fake.phone_number()
-userStatus = randint(1, 2)
-
-print(userName)
-print(password)
-print(email)
-print(phone)
-
-# userName = 'hobbit'
-# password = '123456789'
-# userID = 12
-# firstName =  'Samwise'
-# lastName = 'Gamgee'
-# email = 'modest_guy@shir.org'
-# phone = 'no_phone'
-# userStatus = 1
+user_status = randint(1, 2)
+user_name_updated = fake.user_name()
+password_updated = fake.password()
+email_updated = fake.email()
+phone_updated = fake.phone_number()
 
 headers = {'Content-Type': 'application/json'}
 
-class PostmanTests1(unittest.TestCase):
+class PostmanTests(unittest.TestCase):
 
-	def test_1_get_logIn(self):
-		print('1')
-		payload = json.dumps({'username': userName, 'password': password})
+	def test_1_get_log_in(self):
+		payload = json.dumps({'username': user_name, 'password': password})
 		response = requests.get('https://petstore.swagger.io/v2/user/login', headers = headers, params = payload)
 		self.assertEqual(response.status_code, 200)
 		time.sleep(10)
 
-	def test_2_post_createUser(self):
-		print('2')
-		payload = json.dumps({'id': userID, 'username': userName, 'firstName': firstName, 'lastName': lastName, 'email': email, 'password': password, 'phone': phone, 'userStatus': userStatus})
+	def test_2_post_create_user(self):
+		payload = json.dumps({'id': user_id, 'username': user_name, 'firstName': first_name, 'lastName': last_name, 'email': email, 'password': password, 'phone': phone, 'userStatus': user_status})
 		response = requests.post('https://petstore.swagger.io/v2/user', headers = headers, data = payload)
-		print('request1: ' + response.request.body)
 		self.assertEqual(response.status_code, 200)
+		self.assertEqual(json.loads(response.text)['message'], str(user_id))
 		time.sleep(20)
 
-	def test_3_get_checkCreating(self):
-		print('3')
-		response = requests.get('https://petstore.swagger.io/v2/user/'+ userName)
-		print('response1: ' + response.text)
+	def test_3_get_check_creating(self):
+		response = requests.get('https://petstore.swagger.io/v2/user/'+ user_name)
 		self.assertEqual(response.status_code, 200)
+		self.assertEqual(json.loads(response.text)['id'], user_id)
+		self.assertEqual(json.loads(response.text)['username'], user_name)
+		self.assertEqual(json.loads(response.text)['firstName'], first_name)
+		self.assertEqual(json.loads(response.text)['lastName'], last_name)
+		self.assertEqual(json.loads(response.text)['email'], email)
+		self.assertEqual(json.loads(response.text)['password'], password)
+		self.assertEqual(json.loads(response.text)['phone'], phone)
+		self.assertEqual(json.loads(response.text)['userStatus'], user_status)
 		time.sleep(10)
 
-# userName1 = 'brave_hobbit'
-# password1 = '35464687'
-# email1 = 'winner@shir.org'
-# phone1 = '3546568768'
 
-# userName1 = fake.user_name()
-# password1 = fake.password()
-# email1 = fake.email()
-# phone1 = fake.phone_number()
-
-userName = fake.user_name()
-password = fake.password()
-email = fake.email()
-phone = fake.phone_number()
-
-class PostmanTests2(unittest.TestCase):		
-
-	def test_4_put_updateUser(self):
-		print('4')
-		payload = json.dumps({'id': userID, 'username': userName, 'firstName': firstName, 'lastName': lastName, 'email': email, 'password': password, 'phone': phone, 'userStatus': userStatus})
-		response = requests.put('https://petstore.swagger.io/v2/user/'+ userName, headers = headers, data = payload)
-		print('request2: ' + response.request.body)
+	def test_4_put_update_user(self):
+		payload = json.dumps({'id': user_id, 'username': user_name_updated, 'firstName': first_name, 'lastName': last_name, 'email': email_updated, 'password': password_updated, 'phone': phone_updated, 'userStatus': user_status})
+		response = requests.put('https://petstore.swagger.io/v2/user/'+ user_name_updated, headers = headers, data = payload)
 		self.assertEqual(response.status_code, 200)
-		time.sleep(20)
+		self.assertEqual(json.loads(response.text)['message'], str(user_id))
+		time.sleep(25)
 
-	def test_5_get_checkUpdating(self):
-		print('5')
-		response = requests.get('https://petstore.swagger.io/v2/user/'+ userName1)
-		print('response2: ' + response.text)
-		self.assertEqual(response.status_code, 200) 
+	def test_5_get_check_updating(self):
+		response = requests.get('https://petstore.swagger.io/v2/user/'+ user_name_updated)
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(json.loads(response.text)['id'], user_id)
+		self.assertEqual(json.loads(response.text)['username'], user_name_updated)
+		self.assertEqual(json.loads(response.text)['firstName'], first_name)
+		self.assertEqual(json.loads(response.text)['lastName'], last_name)
+		self.assertEqual(json.loads(response.text)['email'], email_updated)
+		self.assertEqual(json.loads(response.text)['password'], password_updated)
+		self.assertEqual(json.loads(response.text)['phone'], phone_updated)
+		self.assertEqual(json.loads(response.text)['userStatus'], user_status) 
 		time.sleep(10)	
 
-	def test_6_delete_deleteUser(self):
-		print('6')
-		response= requests.delete('https://petstore.swagger.io/v2/user/'+ userName1)
+	def test_6_delete_user(self):
+		response= requests.delete('https://petstore.swagger.io/v2/user/'+ user_name_updated)
 		self.assertEqual(response.status_code, 200)
+		self.assertEqual(json.loads(response.text)['message'], user_name_updated)
 		time.sleep(10)
 
-	def test_7_get_checkAfterDeletion(self):
-		print('7')
-		response = requests.get('https://petstore.swagger.io/v2/user/'+ userName1)
+	def test_7_get_check_after_deletion(self):
+		response = requests.get('https://petstore.swagger.io/v2/user/'+ user_name_updated)
 		self.assertEqual(response.status_code, 404)
+		self.assertEqual(json.loads(response.text)['message'], 'User not found')
 		time.sleep(10)		
 
 		
